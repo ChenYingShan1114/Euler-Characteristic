fce2b2fee6c241ec3c13c9582a9ac362bcd69572
Ying-Shan Chen <cingy1999881114@gmail.com>
2026-05-18 15:07:05 +0800

    done

--- src/edges.cpp
+++ src/edges.cpp
@@ -3,6 +3,43 @@
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
+  E.resize(0, 2);
+
   // ADD YOUR CODE HERE
+
+  // calculate # of edge
+  std::vector<std::pair<int, int>> E_tmp;
+  int start = -1, end = -1;
+  bool undirect_edge_exist = false;
+  for (int i = 0; i < F.rows(); i++) {
+    for (int j = 0; j < 3; j++) {
+      if (j == 2) {
+        start = F(i, j); end = F(i, 0);
+      } else {
+        start = F(i, j); end = F(i, j+1);
+      }
+
+      // find undirect edge exist or not
+      for (int k = 0; k < E_tmp.size(); k++) {
+        undirect_edge_exist = false;
+        if (start == E_tmp[k].second && end == E_tmp[k].first) {
+          E_tmp.erase(E_tmp.begin() + k); 
+
+          // Resize to add one row while keeping existing columns
+          E.conservativeResize(E.rows() + 1, Eigen::NoChange);
+          // Assign values to the new last row (index starts at 0)
+          E.row(E.rows() - 1) << start, end;
+
+          undirect_edge_exist = true;
+          break;
+        }
+      }
+
+      if (!undirect_edge_exist) {
+        E_tmp.push_back({start, end});
+      }
+
+    }
+  }
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,36 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+
+  // calculate # of edge
+  Eigen::MatrixXi E = edges(F);
+
+  // calculate # of vertex
+  std::vector<int> V_tmp;
+  bool exist = false;
+  for (int i = 0; i < F.rows(); i++) {
+    for (int j = 0; j < 3; j++) {
+
+      // test already add or not
+      for (int k = 0; k < V_tmp.size(); k++) {
+        exist = false;
+        if (F(i, j) == V_tmp[k]) {
+          exist = true;
+          break;
+        }
+      }
+      
+      if (!exist) {
+        V_tmp.push_back(F(i, j));
+      }
+
+    }
+  }
+
+  Chi = V_tmp.size() - E.rows() + F.rows();
   return Chi;
 }
